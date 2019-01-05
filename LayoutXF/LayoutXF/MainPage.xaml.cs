78f1df3111050aadf1c2481c8042a278bf14594b
Robson Correia <robsoncorreia01@outlook.com>
2019-01-05 20:23:00 -0200

    Adicionar arquivos de projeto.

---
+++ LayoutXF/LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using LayoutXF.Layouts.Absolute;
+using LayoutXF.Layouts.Grid;
+using LayoutXF.Layouts.Relative;
+using LayoutXF.Layouts.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPageStack(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StackPage());
+        }
+
+        private void GoPageGrid(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GridPage());
+        }
+
+        private void GoPageAbsolute(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AbsolutePage());
+        }
+
+        private void GoPageRelative(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RelativePage());
+        }
+    }
+}
