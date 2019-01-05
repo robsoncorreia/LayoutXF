13be5260e759c0d57186be10c09e10d0ce8ead1a
Robson Correia <robsoncorreia01@outlook.com>
2019-01-05 21:55:41 -0200

    Grid

--- LayoutXF/LayoutXF/MainPage.xaml.cs
+++ LayoutXF/LayoutXF/MainPage.xaml.cs
@@ -4,10 +4,6 @@ using LayoutXF.Layouts.Relative;
 using LayoutXF.Layouts.Scroll;
 using LayoutXF.Layouts.Stack;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace LayoutXF
